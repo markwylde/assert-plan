c2184bc550d11263f38c96c612284d4e90905ade
Mark Wylde <me@markwylde.com>
2022-08-04 21:33:35 +0100

    Fix #2 by converting setInterval to a callback queue

--- test/index.js
+++ test/index.js
@@ -1,8 +1,45 @@
 import test from 'test';
 import assertPlan from '../lib/index.js';
 
-test('test', async t => {
+test('test succeeds with 1 assertion and no timeout', async t => {
   const assert = assertPlan(1);
+
+  assert.strictEqual(1, 1);
+
+  await assert.wait();
+});
+
+test('test fails with 2 assertions when only expecting 1 and no timeout', async t => {
+  const assert = assertPlan(1);
+
+  assert.strictEqual(1, 1);
+  assert.strictEqual(2, 2);
+
+  await assert.wait();
+});
+
+test('test fails with not enough assertions and no timeout', async t => {
+  const assert = assertPlan(2);
+
+  assert.strictEqual(1, 1);
+
+  await assert.wait();
+});
+
+test('test succeeds with enough assertions and an assertPlan timeout', async t => {
+  const assert = assertPlan(2, { timeout: 1000 });
+
+  assert.strictEqual(1, 1);
+
+  setTimeout(() => {
+    assert.strictEqual(2, 2);
+  }, 200);
+
+  await assert.wait();
+});
+
+test('test fails with not enough assertions and an assertPlan timeout', async t => {
+  const assert = assertPlan(2, { timeout: 500 });
 
   assert.strictEqual(1, 1);
 
